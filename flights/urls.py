e3f86d028015836b9e3fee2eff4fc4fea9b60540
KolwaBrad <duncan.bradley@strathmore.edu>
2024-12-17 12:43:21 +0300

    add urls

---
+++ flights/urls.py
@@ -0,0 +1,12 @@
+# flights/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import FlightViewSet, PassengerViewSet
+
+router = DefaultRouter()
+router.register(r'flights', FlightViewSet)
+router.register(r'passengers', PassengerViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
